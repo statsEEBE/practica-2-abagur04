8ec533f6cb6a63c23066347a5e26202f8e36ed0f
ania.bagur <ania.bagur@auleseebe.upc.edu>
2024-10-03 11:28:52 +0200

    Problema1

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,17 @@
 #Codigo para problema 1
 
+# pregunta 1 --> coeficiente de determinación, tenemos ue mirar el
+# multiple R-quared
+
+# pregunta 2 --> hacemos la raíz de de la pregunta 1
+
+# pregunta 3 --> nos estan preguntando la pendiente (variación de y por el
+# incremento de 1 unidad en x), en este caso, es el Estimate assets
+
+# pregunta 4 --> tenemos que sustituir m*647.1+(Intercept)
+
+# pregunta 5 --> nos tenemos que fijar en el coeficiente de deteminación para
+# averiguar la disperisón de los datos respecto la regresión lineal, también
+# sabiendo que la recta va a pasar por el punto calculado en la pregunta
+# anterior
+
